c36fbcd8e1adf597496c0a72c048708590ddb395
Phil Willis <philopian@gmail.com>
2023-04-08 08:55:00 -0700

    add express TS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello TS')
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+})
